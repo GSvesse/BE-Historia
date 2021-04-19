9ef5752a66dd6520bf2cf8232d16b9a060f6fa12
Gsvees <gustaf.svesse@gmail.com>
2021-04-19 12:04:30 +0200

    test java 8

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// hejhejeheasgs<edgs<dg
 @SpringBootApplication
 @RestController
 public class DemoApplication extends SpringBootServletInitializer {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
