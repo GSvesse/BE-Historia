7c489ade0f3324fb16d03c7bc5c9296e4dcba30b
loer4687-2 <nilsfors@me.com>
2021-04-26 11:03:11 +0200

    updaterade tabeller

--- src/main/java/com/example/demo/BildTag.java
+++ src/main/java/com/example/demo/BildTag.java
@@ -1,4 +1,28 @@
+
 package com.example.demo;
 
+import javax.persistence.*;
+
+@Entity(name = "bildTag")
 public class BildTag {
+    @Id
+    private Integer bildId;
+    @Id
+    private Integer tagId;
+
+    public Integer getBildId() {
+        return bildId;
+    }
+
+    public Integer getTagId() {
+        return tagId;
+    }
+
+    public void setBildId(Integer bildId) {
+        this.bildId = bildId;
+    }
+
+    public void setTagId(Integer tagId) {
+        this.tagId = tagId;
+    }
 }
