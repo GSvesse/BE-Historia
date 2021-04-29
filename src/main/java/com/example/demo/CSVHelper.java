d098fd25abf6c7da9d2ff1044f228083046aff6a
loer4687-2 <nilsfors@me.com>
2021-04-29 16:54:22 +0200

    CVS updates

---
+++ src/main/java/com/example/demo/CSVHelper.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.web.multipart.MultipartFile;
+
+public class CSVHelper {
+    public static String TYPE = "text/csv";
+    static String[] HEADERs = { "Id", "Title", "Description", "Published" };
+
+    public static boolean hasCSVFormat(MultipartFile file) {
+
+        return TYPE.equals(file.getContentType());
+    }
+
+    public static List<Bilder> csvToTutorials(InputStream is) {
+        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+             CSVParser csvParser = new CSVParser(fileReader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())) {
+
+            List<Bilder> bildSamling = new ArrayList<>();
+
+            Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+
+            for (CSVRecord csvRecord : csvRecords) {
+                Bilder b = new Bilder();
+                b.setDescription(csvRecord.get("Description"));
+                b.setYear(Integer.parseInt((csvRecord.get("year"))));
+                b.setPhotographer(csvRecord.get("Photographer"));
+                b.setLicence(csvRecord.get("licence"));
+                // hoppar över tag, street och address
+                b.setBlock(csvRecord.get("block"));
+                b.setDistrict(csvRecord.get("district"));
+
+
+                bildSamling.add(b);
+            }
+
+            return bildSamling;
+        } catch (IOException e) {
+            throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/com/example/demo/CSVService.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class CSVService {
+    @Autowired
+    BildRepository bildRepository;
+
+    public void save(MultipartFile file) {
+        try {
+            List<Bilder> bilder = CSVHelper.csvToTutorials(file.getInputStream());
+            bildRepository.saveAll(bilder);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+    }
+
+    public List<Bilder> getAllBilder() {
+        return (List<Bilder>) bildRepository.findAll();
+    }
+}
