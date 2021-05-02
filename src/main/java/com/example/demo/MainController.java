ad578f009122e9250775dae1a57494be9ffc9921
loer4687-2 <nilsfors@me.com>
2021-05-02 17:39:46 +0200

    manual merge

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -11,6 +11,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 //hejhej
@@ -141,7 +143,5 @@ public class MainController {
         }
         return result;
     }
-
-}
 
 }
